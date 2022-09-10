39039ec0fc5c7810fded56c10404a8fe008694c6
Mariia Telehonenko <telegonenko1@ukr.net>
2022-09-11 01:14:52 +0300

    Update ContactsForm.js

--- src/components/ContactsForm/ContactsForm.js
+++ src/components/ContactsForm/ContactsForm.js
@@ -1,66 +1,82 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, FormField, FormBtn } from './ContactsForm.styled';
 import { FormLabelText } from 'components/Components.styled';
-const INITIAL_STATE = {
-  name: '',
-  number: '',
-};
-class ContactsForm extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-  state = { ...INITIAL_STATE };
+// const INITIAL_STATE = {
+//   name: '',
+//   number: '',
+// };
+const CONTACTS_NAME = 'name';
+const CONTACTS_NUMBER = 'number';
+function ContactsForm({ onSubmit }) {
+  // static propTypes = {
+  //   onSubmit: PropTypes.func.isRequired,
+  // };
+  // state = { ...INITIAL_STATE };
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleChange = evt => {
+  const handleChange = evt => {
     const { name, value } = evt.target;
-    this.setState({ [name]: value });
+    // this.setState({ [name]: value });
+    switch (name) {
+      case CONTACTS_NAME:
+        setName(value);
+        break;
+      case CONTACTS_NUMBER:
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
 
-    this.props.onSubmit({ ...this.state });
-    this.reset();
+    onSubmit({ name, number });
+    reset();
   };
 
-  reset = () => {
-    this.setState({ ...INITIAL_STATE });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <FormField>
-          <FormLabelText>Name</FormLabelText>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={name}
-            onChange={this.handleChange}
-          />
-        </FormField>
-        <FormField>
-          <FormLabelText>Number</FormLabelText>
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={number}
-            onChange={this.handleChange}
-          />
-        </FormField>
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormField>
+        <FormLabelText>Name</FormLabelText>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={handleChange}
+        />
+      </FormField>
+      <FormField>
+        <FormLabelText>Number</FormLabelText>
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={handleChange}
+        />
+      </FormField>
 
-        <FormBtn type="submit">Add contact</FormBtn>
-      </Form>
-    );
-  }
+      <FormBtn type="submit">Add contact</FormBtn>
+    </Form>
+  );
 }
+
+ContactsForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default ContactsForm;
